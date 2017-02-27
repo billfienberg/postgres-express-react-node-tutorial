99863dd3a7a2961396f884c4677943b938163e10
Bill Fienberg <wfienberg2@hotmail.com>
2017-02-27 08:40:38 -0600

    Update todo.js from ES5 to ES6 and add a not-null constraint

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,13 +1,19 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define('Todo', {
+    title: {
+      DataTypes.STRING,
+      allowNull: false,
+    },
   }, {
     classMethods: {
-      associate: function(models) {
+      associate: (models) => {
         // associations can be defined here
-      }
-    }
+        Todo.hasMany(models.TodoItem, {
+          foreignKey: 'todoId',
+          as: 'todoItems',
+        });
+      },
+    },
   });
   return Todo;
 };
