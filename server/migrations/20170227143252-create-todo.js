79b1e4571d11c32ec29a728b4ee2f6608aeb17e4
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-01 06:31:55 -0600

    Remove unnecessary return from arrow function in 20170227143252-create-todo.js

--- server/migrations/20170227143252-create-todo.js
+++ server/migrations/20170227143252-create-todo.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    return queryInterface.createTable('Todos', {
+    queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
