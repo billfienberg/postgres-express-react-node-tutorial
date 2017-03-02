c9b11454ecc8089f0b47513731f6de63c613b676
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-02 08:20:40 -0600

    Add index route

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const todosController = require('../controllers').todos;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Todos API!',
+  }));
+
+  app.post('/api/todos', todosController.create);
+};
