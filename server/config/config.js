61507897c824d838b6caa637a6d581ca0c91e95a
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-02 04:45:31 -0600

    Update database config and references to config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,13 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "use_env_variable": "DATABASE_URL"
+    "dialect": process.env.DATABASE_DIALECT,
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
+    "database": process.env.DATABASE_NAME
   },
   "test": {
     "username": "root",
--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,12 @@
 'use strict';
-
 const fs        = require('fs');
 const path      = require('path');
 const Sequelize = require('sequelize');
 const basename  = path.basename(module.filename);
 const env       = process.env.NODE_ENV || 'development';
-const config    = require(`${__dirname}/../config/config.json`)[env];
+const config    = require(`${__dirname}/../config/config`)[env];
 const db        = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  // From the environment, extract the key with the name provided in the config as use_env_variable
-  // and use that to establish a connection to our database.
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs
   .readdirSync(__dirname)
