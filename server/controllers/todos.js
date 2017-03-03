1f57a1ee4d212b79796c0912d3e419e3f0667d2b
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-03 08:50:22 -0600

    Update router handler (list) to include an array of TodoItems with each Todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
   create(req, res) {
@@ -11,8 +12,13 @@ module.exports = {
   },
   list(req, res) {
     return Todo
-      .all()
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
       .then(todos => res.status(200).send(todos))
-      .catch(err => res.status(400).send(error));
+      .catch(error => res.status(400).send(error));
   },
 };
