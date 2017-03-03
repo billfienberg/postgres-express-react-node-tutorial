fe747d59f071458ee82b56fba00c91a9c3abbb00
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-02 23:01:06 -0600

    Add route for POST /api/todos/:todoId/items

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -7,4 +8,5 @@ module.exports = (app) => {
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
+  app.post('/api/todos/:todoId/items', todoItemsController.create)
 };
