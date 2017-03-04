4bfbffe8c7979d51471812286e3a45468a66334a
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-04 14:53:55 -0600

    Add PUT /api/todos/:todoId and route handler (update) to update a single Todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -39,4 +39,27 @@ module.exports = {
       })
       .catch(error => res.status(400).send(error));
   },
+  update(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,5 +9,6 @@ module.exports = (app) => {
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
   app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
   app.post('/api/todos/:todoId/items', todoItemsController.create)
 };
