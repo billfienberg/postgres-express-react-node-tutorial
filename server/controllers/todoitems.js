d447de3529a4128e0555462cf50798b0c8e17d5a
Bill Fienberg <wfienberg2@hotmail.com>
2017-03-04 15:10:33 -0600

    Add PUT and DELETE /api/todos/:todoId/items/:todoItemId and route handlers (update, destroy) to update or delete a single TodoItem

--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -10,4 +10,50 @@ module.exports = {
       .then(todoItem => res.status(201).send(todoItem))
       .catch(error => res.status(400).send(error));
   },
+  update(req, res) {
+    return TodoItem
+      .find({
+        where: {
+          id: req.params.todoItemId,
+          todoId: req.params.todoId,
+        },
+      })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({
+            message: 'TodoItem Not Found',
+          });
+        }
+
+        return todoItem
+          .update({
+            content: req.body.content || todoItem.content,
+            complete: req.body.complete || todoItem.complete,
+          })
+          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+          .catch(error => res.status(400).send(error));
+      }),
+      destroy(req, res) {
+        return todoItem
+          .find({
+            where: {
+              id: req.params.todoItemId,
+              todoId: req.params.todoId,
+            },
+          })
+          .then(todoItem => {
+            if (!todoItem) {
+              return res.status(404).send({
+                message: 'TodoItem Not Found',
+              });
+            }
+
+            return todoItem
+              .destroy()
+              .then(() => res.status(204).send())
+              .catch(error => res.status(400).send(error));
+          })
+          .catch(error => res.status(400).send(error));
+      },
+  }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,5 +11,14 @@ module.exports = (app) => {
   app.get('/api/todos/:todoId', todosController.retrieve);
   app.put('/api/todos/:todoId', todosController.update);
   app.delete('/api/todos/:todoId', todosController.destroy);
+
   app.post('/api/todos/:todoId/items', todoItemsController.create)
+  app.put('/api,todos/:todoId/items/:todoItemId', todoItemsController.update);
+  app.delete('/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy);
+
+  app.all('/api/todos/:todoId/items', (req, res) => {
+    res.status(405).send({
+      message: 'Method Not Allowed',
+    })
+  });
 };
